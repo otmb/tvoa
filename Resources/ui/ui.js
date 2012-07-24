603d06a53f464ab8adfeb0549e13c1a5ebf180ba
Manabu <manabu@o2air.local>
2012-07-25 05:42:13 +0900

    translate and sound save fix

--- Resources/ui/ui.js
+++ Resources/ui/ui.js
@@ -43,16 +43,18 @@
           return;
         }
         response.data.item.forEach(function(item){
-          var link = item.link.match(/[0-9]+.html$/);
-          link = 'http://m.voanews.com/learningenglish/' + link + '?show=full';
-          tableView.appendRow({title: item.title, color: '#000', link: link, hasChild: true});
+          var pageid = item.link.match(/[0-9]+.html$/)[0];
+          link = 'http://m.voanews.com/learningenglish/' + pageid + '?show=full';
+          pageid = pageid.match(/[0-9]+/)[0];
+          tableView.appendRow({title: item.title, color: '#000', link: link, pageid: pageid, hasChild: true});
         });
       });
     });
     
     // List View Create
     tableView.addEventListener("click",function(evt){
-      
+      app.ui.webview = 0;
+      app.ui.url = evt.rowData.link;
       // before sound remove
       var sound = app.ui.sound;
       if (sound){
@@ -82,7 +84,11 @@
           alert("Yahoo YQL error.");
           return;
         }
-        var text = response.data.p.content;
+        var contents = response.data.p.content;
+        contents = contents.replace(/  /g,"");
+        contents = contents.replace(/\n\n/g,"\n");
+        //console.log(contents);
+        var text = evt.rowData.title + "\n\n" + contents;
         var scrollView = Titanium.UI.createScrollView({
           contentWidth:'auto',
           contentHeight:'auto',
@@ -105,148 +111,160 @@
         view1.add(scrollView);
       });
       
-      // mp3 download
-      var xpath = '//a[contains(text(), \"Listen to this article\")]/@href';
-      var query = String.format("select * from html where url = '%s' and xpath='%s'",evt.rowData.link,xpath);
-      Ti.Yahoo.yql(query,function(response){
-        if (response.success === false){
-          alert("Yahoo YQL error.");
-          return;
-        }
-        var link = response.data.a.href;
+      //Ti.API.info('directoryListing = ' + filePath.getParent().getDirectoryListing());
+      createSound = function(filePath) {
+        sound = Ti.Media.createSound({
+          url: filePath,
+          allowBackground: true
+        });
+        app.ui.sound = sound;
         
-        var mp3 = Ti.Network.createHTTPClient();
-        mp3.open('GET',link);
-        mp3.onload  = function() {
-            var filePath;
-            if (Ti.Platform.osname === 'android'){
-              filePath = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory,"hoge.mp3");
-            } else {
-              filePath = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, "hoge.mp3"); 
+        var startStopButton = Ti.UI.createButton({
+          title:'Start/Stop',
+          top: 10,
+          width:200,
+          height:40
+        });
+        
+        view1.add(startStopButton);
+        // test
+        //sound.setTime(100);
+        
+        startStopButton.addEventListener("click",function() {
+          if (sound.playing){
+            //console.log("paused");
+            sound.pause();
+            //if (Ti.Platform.osname === 'android'){
+            //  sound.release();
+            //}
+          } else {
+            //console.log("played");
+            sound.play();
+          }
+        });
+        
+        sound.addEventListener('complete', function()
+        {
+          if (Ti.Platform.osname === 'android')
+          {
+            sound.release();
+          }
+        });
+        
+        if (Ti.Platform.osname === 'iphone'){
+          var slider = Ti.UI.createSlider({
+            top: 50,
+            min: 0,
+            //max: 100,
+            width: '90%',
+          });
+          
+          var label = Ti.UI.createLabel({
+            text: slider.value,
+            width: '100%',
+            height: 'auto',
+            top: 30,
+            left: 0,
+            textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
+          });
+          
+          // slider plus minus 2 change
+          slider.addEventListener('change', function(e) {
+             
+            if (e.value > sound.getTime() + 2 || e.value < sound.getTime() - 2 ){
+              //slider.value = e.value;
+              sound.setTime(e.value);
+              console.log("slide: " + e.value);
+              console.log("sound: " + sound.getTime());
             }
-            filePath.write(this.responseData);
-            
-            //Ti.API.info('directoryListing = ' + filePath.getParent().getDirectoryListing());
-           
-            sound = Ti.Media.createSound({
-              url: filePath,
-              allowBackground: true
-            });
-            app.ui.sound = sound;
-            
-            var startStopButton = Ti.UI.createButton({
-              title:'Start/Stop',
-              top: 10,
-              width:200,
-              height:40
-            });
-            
-            view1.add(startStopButton);
-            // test
-            //sound.setTime(100);
-            
-            startStopButton.addEventListener("click",function() {
-              if (sound.playing){
-                //console.log("paused");
-                sound.pause();
-                //if (Ti.Platform.osname === 'android'){
-                //  sound.release();
-                //}
-              } else {
-                //console.log("played");
-                sound.play();
-              }
-            });
-            
-            sound.addEventListener('complete', function()
-            {
-              if (Ti.Platform.osname === 'android')
-              {
-                sound.release();
-              }
-            });
-            
-            if (Ti.Platform.osname === 'iphone'){
-              var slider = Ti.UI.createSlider({
-                top: 50,
-                min: 0,
-                //max: 100,
-                width: '90%',
-              });
-              
-              var label = Ti.UI.createLabel({
-                text: slider.value,
-                width: '100%',
-                height: 'auto',
-                top: 30,
-                left: 0,
-                textAlign: Ti.UI.TEXT_ALIGNMENT_CENTER
-              });
-              
-              // slider plus minus 2 change
-              slider.addEventListener('change', function(e) {
-                 
-                if (e.value > sound.getTime() + 2 || e.value < sound.getTime() - 2 ){
-                  //slider.value = e.value;
-                  sound.setTime(e.value);
-                  console.log("slide: " + e.value);
-                  console.log("sound: " + sound.getTime());
-                }
-              });
-            
-              slider.max = sound.getDuration();
-              slider.value = sound.getTime();
-              view1.add(slider);
-              
-            } else { // android progress
-              /*
-              var pb=Titanium.UI.createProgressBar({
-                top: 50,
-                width:"100%",
-                height:'auto',
-                min:0,
-                max: sound.duration,
-                //color:'#fff',
-                //message:'Downloading 0 of 10',
-                //font:{fontSize:14, fontWeight:'bold'},
-                style:Ti.UI.iPhone.ProgressBarStyle.PLAIN,
-              });
-              //pb.max = sound.getDuration();
-              //pb.value = Math.round(sound.getTime() / sound.getDuration()*1000)/1000;
-              view1.add(pb);
-              pb.show();
-              console.log("pb max: "+ sound.duration);
-              */
-            }
-            if (Ti.Platform.osname === 'iphone'){
-              var i = setInterval(function()
-              {
-                if (sound.playing)
-                {
-                  //Ti.API.info('time ' + sound.getTime());
-                  if (Ti.Platform.osname === 'iphone'){
-                    slider.value = sound.getTime();
-                  } else {
-                    // micro sec
-                    //pb.value = sound.getTime();
-                    //pb.value = Math.round(sound.getTime() / sound.getDuration()*1000)/1000;
-                    //console.log(pb.value);
-                  }
-                }
-              },500);
-            }
-            detailWin.addEventListener('close',function() {
-              sound.stop();
-              if (Ti.Platform.osname === 'android')
-              {
-                sound.release();
-              } else {
-                clearInterval(i);
-              }
-            });
+          });
+        
+          slider.max = sound.getDuration();
+          slider.value = sound.getTime();
+          view1.add(slider);
+          
+        } else { // android progress
+          /*
+          var pb=Titanium.UI.createProgressBar({
+            top: 50,
+            width:"100%",
+            height:'auto',
+            min:0,
+            max: sound.duration,
+            //color:'#fff',
+            //message:'Downloading 0 of 10',
+            //font:{fontSize:14, fontWeight:'bold'},
+            style:Ti.UI.iPhone.ProgressBarStyle.PLAIN,
+          });
+          //pb.max = sound.getDuration();
+          //pb.value = Math.round(sound.getTime() / sound.getDuration()*1000)/1000;
+          view1.add(pb);
+          pb.show();
+          console.log("pb max: "+ sound.duration);
+          */
         }
-        mp3.send();
-      });
+        if (Ti.Platform.osname === 'iphone'){
+          var i = setInterval(function()
+          {
+            if (sound.playing)
+            {
+              //Ti.API.info('time ' + sound.getTime());
+              if (Ti.Platform.osname === 'iphone'){
+                slider.value = sound.getTime();
+              } else {
+                // micro sec
+                //pb.value = sound.getTime();
+                //pb.value = Math.round(sound.getTime() / sound.getDuration()*1000)/1000;
+                //console.log(pb.value);
+              }
+            }
+          },500);
+        }
+        detailWin.addEventListener('close',function() {
+          sound.stop();
+          if (Ti.Platform.osname === 'android')
+          {
+            sound.release();
+          } else {
+            clearInterval(i);
+          }
+        });
+      };
+      
+      // mp3 download
+      var imageDir;
+      var fname = evt.rowData.pageid + ".mp3";
+       if (Ti.Platform.osname === 'android'){
+        soundDir = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory, 'sound');
+      } else {
+        soundDir = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, 'sound');
+      }
+      if (!soundDir.exists()) {
+        soundDir.createDirectory();
+      }
+      var filePath = Ti.Filesystem.getFile(soundDir.nativePath , evt.rowData.pageid + ".mp3");
+      console.log("SoundFile path is: " + filePath.resolve());
+      if (filePath.exists()){
+        createSound(filePath.nativePath);
+      } else {
+        var xpath = '//a[contains(text(), \"Listen to this article\")]/@href';
+        var query = String.format("select * from html where url = '%s' and xpath='%s'",evt.rowData.link,xpath);
+        Ti.Yahoo.yql(query,function(response){
+          if (response.success === false){
+            alert("Yahoo YQL error.");
+            return;
+          }
+          
+          var link = response.data.a.href;
+          var mp3 = Ti.Network.createHTTPClient();
+          mp3.open('GET',link);
+          mp3.onload  = function() {
+              filePath.write(this.responseData);
+              createSound(filePath.nativePath);
+          }
+          mp3.send();
+        });
+      }
       
       var scrollView = Titanium.UI.createScrollableView({
         views: [view1,view2],
@@ -256,6 +274,21 @@
       });
       detailWin.add(scrollView);
       
+      scrollView.addEventListener('scroll', function(e){
+        
+        if (e.currentPage === 1 && app.ui.webview === 0){
+          var webview = Titanium.UI.createWebView();
+          //webview.url = "http://translate.google.co.jp/translate?twu=1&sl=en&tl=ja&hl=ja&sc=1&q=" 
+          //webview.url = "http://translate.google.co.jp/#en|ja|" 
+          webview.url = "http://translate.google.co.jp/translate?twu=1&q=hello?sl=en&tl=ja&u="
+            + encodeURIComponent(app.ui.url);
+          webview.scalesPageToFit = true;
+          webview.width = "100%";
+          view2.add(webview);
+          app.ui.webview = 1;
+        }
+        
+      });
       tab.open(detailWin);
       
     });
