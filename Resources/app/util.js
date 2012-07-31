6297a6c5da4e9b3f8bc1d28045f7b3832dacf24d
Manabu <manabu@o2air.local>
2012-08-01 02:17:17 +0900

    code Refactoring

---
+++ Resources/app/util.js
@@ -0,0 +1,28 @@
+(function(){
+  
+  app.util = {};
+  
+  app.util.getPath = function(folder){
+    var path;
+    if (Ti.Platform.osname === 'android'){
+      path = Ti.Filesystem.getFile(Ti.Filesystem.externalStorageDirectory, folder);
+    } else {
+      path = Ti.Filesystem.getFile(Ti.Filesystem.applicationDataDirectory, folder);
+    }
+    if (!path.exists()) {
+      path.createDirectory();
+    }
+    return path.nativePath;
+  };
+  
+  app.util.getYql = function(url,xpath,option){
+    var query = String.format("select * from html where url = '%s' and xpath='%s'",url,xpath);
+    console.log(query);
+    Ti.Yahoo.yql(query,function(response){
+      if (response.success === false || !response.data){
+        throw "error:Page Loading Error.";
+      }
+      option.callback(response.data,option);
+    });
+  };
+})();
